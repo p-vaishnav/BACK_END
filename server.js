0f1e005328f5f5fdd3b7bb27ff21df5c5375f32a
vaishnav <vaishnnav91@gmail.com>
2021-01-26 00:53:21 +0530

    new

--- server.js
+++ server.js
@@ -3,30 +3,15 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const path = require('path');
-const cors = require('cors');
 const employees = require('./routes/api/employees');
 const orders = require('./routes/api/orders');
 
 const app = express();
-let whitelist = ['http://localhost:3000/', 'https://svp-proj-caf-final.herokuapp.com/']
-let corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.use(cors(corsOptions));
 
 app.use(passport.initialize());
-
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-
 app.use(bodyParser.json({limit: '25MB'}));
 
 require('./config/passportConfig')(passport);
